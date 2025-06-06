d550e27660710df4b34092bbf004c8ecb80fd418
huynthe176170 <157361985+huynthe176170@users.noreply.github.com>
2025-06-06 16:03:24 +0700

    frontend login and register

--- SEP490-SU25-G86-Client/Program.cs
+++ SEP490-SU25-G86-Client/Program.cs
@@ -6,16 +6,16 @@ namespace SEP490_SU25_G86_Client
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container.  
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure the HTTP request pipeline.  
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
                 app.UseHsts();
             }
 
@@ -25,6 +25,13 @@ namespace SEP490_SU25_G86_Client
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // Map default route to Login.cshtml  
+            app.MapGet("/", context =>
+            {
+                context.Response.Redirect("/CommonScreen/Login");
+                return Task.CompletedTask;
+            });
 
             app.MapRazorPages();
 
