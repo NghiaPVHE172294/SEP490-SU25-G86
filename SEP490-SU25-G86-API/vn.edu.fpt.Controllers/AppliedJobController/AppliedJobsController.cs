a1c17477b7e4d974b464160f07a81ca0e848b45c
huynthe176170 <157361985+huynthe176170@users.noreply.github.com>
2025-06-20 15:30:59 +0700

    Change Authorize

--- SEP490-SU25-G86-API/vn.edu.fpt.Controllers/AppliedJobController/AppliedJobsController.cs
+++ SEP490-SU25-G86-API/vn.edu.fpt.Controllers/AppliedJobController/AppliedJobsController.cs
@@ -9,7 +9,7 @@ namespace SEP490_SU25_G86_API.vn.edu.fpt.Controllers.AppliedJobController
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize(Roles = "CANDIDATE")]
+    [Authorize]
     public class AppliedJobsController : ControllerBase
     {
         private readonly IAppliedJobService _appliedJobService;
