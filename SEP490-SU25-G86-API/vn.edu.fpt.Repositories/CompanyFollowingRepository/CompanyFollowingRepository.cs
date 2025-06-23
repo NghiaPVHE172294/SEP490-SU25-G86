8a788348c636ef36a516dcf7bd3455b6778fa0b3
khacchien1411 <chiennkhe161554@fpt.edu.vn>
2025-06-23 10:57:05 +0700

    company fl

---
+++ SEP490-SU25-G86-API/vn.edu.fpt.Repositories/CompanyFollowingRepository/CompanyFollowingRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEP490_SU25_G86_API.Models;
+using SEP490_SU25_G86_API.vn.edu.fpt.DTO.CompanyFollowingDTO;
+
+namespace SEP490_SU25_G86_API.vn.edu.fpt.Repositories.CompanyFollowingRepositories
+{
+    public class CompanyFollowingRepository : ICompanyFollowingRepository
+    {
+        private readonly SEP490_G86_CvMatchContext _context;
+
+        public CompanyFollowingRepository(SEP490_G86_CvMatchContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<CompanyFollowingDTO>> GetFollowedCompaniesByUserIdAsync(int userId)
+        {
+            return await _context.CompanyFollowers
+                .Where(cf => cf.UserId == userId && cf.IsActive == true)
+                .Include(cf => cf.Company)
+                .ThenInclude(c => c.Industry)
+                .Select(cf => new CompanyFollowingDTO
+                {
+                    CompanyId = cf.CompanyId,
+                    CompanyName = cf.Company.CompanyName,
+                    LogoUrl = cf.Company.LogoUrl,
+                    Website = cf.Company.Website,
+                    IndustryName = cf.Company.Industry.IndustryName,
+                    Description = cf.Company.Description,
+                    FlowedAt = cf.FlowedAt
+                })
+                .ToListAsync();
+        }
+    }
+}
