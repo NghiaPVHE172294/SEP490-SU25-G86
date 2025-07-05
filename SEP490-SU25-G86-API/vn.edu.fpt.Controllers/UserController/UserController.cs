021f3abe65dae1e24f5ee8f2e17b6518cc5ee8f1
Vu Ngoc Nhat <nhatvnhe171071@fpt.edu.vn>
2025-07-05 17:14:03 +0700

    Fixed update profile

--- SEP490-SU25-G86-API/vn.edu.fpt.Controllers/UserController/UserController.cs
+++ SEP490-SU25-G86-API/vn.edu.fpt.Controllers/UserController/UserController.cs
@@ -29,7 +29,7 @@ namespace SEP490_SU25_G86_API.vn.edu.fpt.Controllers.UserController
         }
 
         [HttpPut("profile")]
-        public async Task<IActionResult> UpdateProfile([FromBody] UserProfileDTO dto)
+        public async Task<IActionResult> UpdateProfile([FromBody] UpdateUserProfileDTO dto)
         {
             var accountId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "0");
             if (accountId == 0) return Unauthorized();
